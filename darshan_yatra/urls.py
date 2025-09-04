0514c133b9d23d3118918883fd50478d256d8d92
Praphull Subhash Sangle <sangalepraphulla9689@gmail.com>
2025-09-04 11:27:31 +0530

    PS merge in main

--- darshan_yatra/urls.py
+++ darshan_yatra/urls.py
@@ -13,5 +13,15 @@ urlpatterns = [
     # path("route_yatra_api/", views.route_yatra_api, name="route_yatra_api"),
     path("logout/", views.logout, name="logout"),
 
+    path('route_master/', views.route_master, name='route_master'),
+    path('route_master_api/', views.route_master_api, name='route_master_api'),
+
+    path('area_master/', views.area_master, name='area_master'),
+    path('area_master_api/', views.area_master_api, name='area_master_api'),
+
+
+    path('yatra_master/', views.yatra_master, name='yatra_master'),
+    path('yatra_master_api/', views.yatra_master_api, name='yatra_master_api'),
+
         
 ]
